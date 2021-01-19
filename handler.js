aa111d425f0addb11cc4f9a732c82061ac1c0176
Filipe Leuch Bonfim <filipe1309@gmail.com>
2021-01-18 23:14:02 -0300

    feat: add endpoint to get one specific user

--- handler.js
+++ handler.js
@@ -7,7 +7,7 @@ const pacientes = [
 ];
 
 module.exports.listarPacientes = async(event) => {
-    console.log(event);
+    // console.log(event);
     return {
         statusCode: 200,
         body: JSON.stringify({
@@ -17,7 +17,14 @@ module.exports.listarPacientes = async(event) => {
             2
         ),
     };
+};
 
-    // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-    // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+module.exports.obterPaciente = async(event) => {
+    const { pacienteId } = event.pathParameters;
+    const paciente = pacientes.find(paciente => paciente.id == pacienteId);
+
+    return {
+        statusCode: 200,
+        body: JSON.stringify(paciente, null, 2),
+    };
 };
